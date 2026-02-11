749dcbeedc93024d1ef90ce3c48975ecc4128477
ralmetov6-cell <ralmetov6@gmail.com>
2026-02-11 15:05:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OOP_Lab1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Student st = new Student(
+				"Альметов Радмир Рамильевич",
+				"ИСЭбд-11",
+				1,
+				4, 5, 3, 5, 4
+			);
+
+			
+			Console.WriteLine("ФИО: " + st.FullName);
+			Console.WriteLine("Группа: " + st.Group);
+			Console.WriteLine("Курс: " + st.Course);
+			Console.WriteLine("Оценки: " + string.Join(", ", st.Grades));
+
+
+			Console.WriteLine("Средний балл: " + st.GetAverage());
+
+			
+			int min, max;
+			st.GetMinMax(out min, out max);
+
+			Console.WriteLine("Минимальная оценка: " + min);
+			Console.WriteLine("Максимальная оценка: " + max);
+
+			
+			st.NextCourse();
+			Console.WriteLine("\nПосле перевода на следующий курс:");
+			Console.WriteLine("Курс: " + st.Course);
+		}
+	}
+}
---
+++ Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace OOP_Lab1
+{
+	class Student
+	{
+		
+		private string fullName;
+		private string group;
+		private int course;
+		private int[] grades;
+
+		
+		public string FullName
+		{
+			get { return fullName; }
+			set { fullName = value; }
+		}
+
+		public string Group
+		{
+			get { return group; }
+			set { group = value; }
+		}
+
+		public int Course
+		{
+			get { return course; }
+			set { course = value; }
+		}
+
+		public int[] Grades
+		{
+			get { return grades; }
+			set { grades = value; }
+		}
+
+		
+		public Student(string name, string groupName, int courseNumber, params int[] marks)
+		{
+			FullName = name;
+			Group = groupName;
+			Course = courseNumber;
+			Grades = marks;
+		}
+
+		
+		public double GetAverage()
+		{
+			return Grades.Average();
+		}
+
+		
+		public void NextCourse()
+		{
+			Course++;
+		}
+
+		
+		public void GetMinMax(out int min, out int max)
+		{
+			min = Grades.Min();
+			max = Grades.Max();
+		}
+	}
+}
